f7ff37cf009378f1f881a437257d1aaac7f008c1
VCrashreal <vcrashreal@gmail.com>
2025-04-09 20:57:12 +0300

    server.py

--- server.py
+++ server.py
@@ -77,4 +77,5 @@ def get_profile():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # получаем порт из переменных окружения
+    app.run(host="0.0.0.0", port=port)  # слушаем на всех интерфейсах
